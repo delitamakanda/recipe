13dd0c7fcdbf483a15525ef512c8017253a3096b
Délita Makanda <delita.makanda@gmail.com>
2024-08-24 00:26:58 +0200

    chore: add recipe to admin

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,12 +1,12 @@
 from .models import Recipe
 from .serializers import RecipeSerializer
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_serializer_class(self):
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
