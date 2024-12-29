ab611a9f85a7ca7df2660026ad02f298c94d2030
Délita Makanda <delita.makanda@gmail.com>
2024-12-29 21:34:19 +0100

    add line to bottom

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { enhancedImages} from '@sveltejs/enhanced-img';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
-import { env } from '$env/dynamic/private';
-
-const vitestPath = env.VITE_VITEST;
 
 export default defineConfig({
-	plugins: [sveltekit(), enhancedImages()],
-	define: {
-		'import.meta.env.VITE_VITEST': JSON.stringify(vitestPath),
-	}
+	plugins: [sveltekit(), enhancedImages()]
 });
