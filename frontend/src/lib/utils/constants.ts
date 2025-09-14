3a1029f374097d7215a8a2429215099ebc7c0062
Délita Makanda <delita.makanda@gmail.com>
2025-09-14 22:24:12 +0200

    remove(constants): remove unused `BASE_API_URL` constant

--- frontend/src/lib/utils/constants.ts
+++ frontend/src/lib/utils/constants.ts
@@ -1,9 +1,7 @@
-import type { Variables } from "$lib/interfaces/variables.interface";
+import type { Variables } from '$lib/interfaces/variables.interface';
 
-const BASE_API_URL: string = import.meta.env.DEV ? import.meta.env.VITE_BASE_API_URI_DEV : import.meta.env.VITE_BASE_API_URI_PROD;
 const RECIPES_PER_PAGE: number = 10;
 
 export const variables: Variables = {
-    BASE_API_URL: BASE_API_URL,
-    RECIPES_PER_PAGE: RECIPES_PER_PAGE,
-}
+	RECIPES_PER_PAGE: RECIPES_PER_PAGE
+};
