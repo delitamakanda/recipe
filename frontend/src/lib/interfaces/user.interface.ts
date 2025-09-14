3f32a3d6e6d1e5e81b731f38b290b3fe67135d92
Délita Makanda <delita.makanda@gmail.com>
2025-09-14 22:19:21 +0200

    feat(auth): add `isAuthenticated` store and sync with localStorage
    Implemented a writable Svelte store to manage authentication state. Updates authentication status based on localStorage and user data changes. Modified `User` interface to use `id` as a string instead of a number.

--- frontend/src/lib/interfaces/user.interface.ts
+++ frontend/src/lib/interfaces/user.interface.ts
@@ -1,22 +1,22 @@
 export interface Token {
-    access: string;
-    refresh: string;
+	access: string;
+	refresh: string;
 }
 
 export interface User {
-    id: number;
-    username: string;
-    email: string;
-    password: string;
-    first_name: string;
-    last_name: string;
-    is_staff: boolean;
-    is_superuser: boolean;
-    date_joined: Date;
-    last_login: Date;
-    tokens: Token;
+	id: string;
+	username: string;
+	email: string;
+	password: string;
+	first_name: string;
+	last_name: string;
+	is_staff: boolean;
+	is_superuser: boolean;
+	date_joined: Date;
+	last_login: Date;
+	tokens: Token;
 }
 
 export interface UserResponse {
-    user: User;
+	user: User;
 }
--- frontend/src/lib/store/user.ts
+++ frontend/src/lib/store/user.ts
@@ -1,4 +1,20 @@
-import type { User } from "$lib/interfaces/user.interface";
-import { writable } from "svelte/store";
+import type { User } from '$lib/interfaces/user.interface';
+import { writable } from 'svelte/store';
+import { browser } from '$app/environment';
 
 export const userData = writable<User>({} as User);
+
+export const isAuthenticated = writable<boolean>(false);
+
+if (browser) {
+	const storedToken = localStorage.getItem('token');
+	if (storedToken === 'true') {
+		isAuthenticated.set(true);
+	}
+	isAuthenticated.subscribe((isAuthenticated) => {
+		localStorage.setItem('token', String(isAuthenticated));
+	});
+	userData.subscribe((userData) => {
+		isAuthenticated.set(!!userData);
+	});
+}
