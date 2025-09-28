4ba951d5e7f4a5b687d35f49f941dacb6d69337b
Délita Makanda <delita.makanda@gmail.com>
2025-09-28 21:48:16 +0200

    remove(header): simplify navigation by removing user-specific logic
    Eliminated user-dependent navigation links and logout functionality from the Header component. This aligns with recent updates to user authentication and account management processes.

--- frontend/src/lib/components/Header/Header.svelte
+++ frontend/src/lib/components/Header/Header.svelte
@@ -1,30 +1,13 @@
 <script lang="ts">
-    import { page } from "$app/stores";
-    import { userData } from "$lib/store/user";
-    import { logoutUser } from "$lib/utils/requestUtils";
+	import { page } from '$app/stores';
 </script>
 
 <header>
-    <nav>
-        <ul>
-            <li class:active={$page.url.pathname=== '/'}>
-                <a href="/">Home</a>
-            </li>
-            {#if !$userData?.username}
-            <li class:active={$page.url.pathname=== '/accounts/login'}>
-                <a href="/accounts/login">Login</a>
-            </li>
-            <li class:active={$page.url.pathname=== '/accounts/register'}>
-                <a href="/accounts/register">Signup</a>
-            </li>
-            {:else}
-            <li>
-                <a href="/accounts/user/{$userData.username}-{$userData.id}">{$userData.username}</a>
-            </li>
-            <li>
-                <a href={null} on:click={logoutUser}>Logout</a>
-            </li>
-            {/if}
-        </ul>
-    </nav>
+	<nav>
+		<ul>
+			<li class:active={$page.url.pathname === '/'}>
+				<a href="/">Home</a>
+			</li>
+		</ul>
+	</nav>
 </header>
