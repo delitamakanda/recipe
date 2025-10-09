c2823f0dcf9dcfe50ec61bd40503f3fe021a6f7b
Délita Makanda <delita.makanda@gmail.com>
2025-10-09 23:18:20 +0200

    refactor(layout): improve footer link formatting and update SVG attributes for consistency

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -55,6 +55,11 @@
 					xmlns="http://www.w3.org/2000/svg"
 					class="h-6 w-6 mr-2"
 					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round">
 					stroke="currentColor">
 					<path
 						stroke-linecap="round"
