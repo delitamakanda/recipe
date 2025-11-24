db2e0a9600df5eb654be373aef0f5ebefbcf99b4
Délita Makanda <delitamakanda@users.noreply.github.com>
2025-11-24 22:05:41 +0100

    Increase RECIPES_PER_PAGE constant from 5 to 10

--- frontend/src/lib/utils/constants.ts
+++ frontend/src/lib/utils/constants.ts
@@ -1,6 +1,6 @@
 import type { Variables } from '$lib/interfaces/variables.interface';
 
-const RECIPES_PER_PAGE: number = 5;
+const RECIPES_PER_PAGE: number = 10;
 
 export const variables: Variables = {
 	RECIPES_PER_PAGE: RECIPES_PER_PAGE,
